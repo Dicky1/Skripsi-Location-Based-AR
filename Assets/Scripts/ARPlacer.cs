d5cf184709ba28572b399fdd5333399db5e5179d
dicky <dsetiono1@gmail.com>
2020-12-14 18:03:01 +0700

    skripsi

---
+++ Assets/Scripts/ARPlacer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.XR;
+using UnityEngine.XR.ARFoundation;
+
+public class ARPlacer : MonoBehaviour
+{
+   ARRaycastManager m_ARRaycastManager;
+   
+   List<ARRaycastHit> raycast_hits = new List<ARRaycastHit>();
+
+
+   public GameObject LocationBasedARPrefab;
+
+   private void Awake()
+   {
+     m_ARRaycastManager = GetComponent<ARRaycastManager>();
+   }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0) 
+	{
+	   Touch touch = Input.GetTouch(0);
+	   if (m_ARRaycastManager.Raycast(touch.position, raycast_hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+     {
+      Pose pose = raycast_hits[0].pose;     
+      LocationBasedARPrefab.transform.position = pose.position;
+
+	   }
+   }
+ }
+}
---
+++ Assets/Scripts/PlaneController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.UI;
+
+public class PlaneController : MonoBehaviour
+{
+    ARPlaneManager m_ARPlaneManager;
+
+    public Text buttonText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_ARPlaneManager = GetComponent<ARPlaneManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TogglePlaneDetectionAndVisibility()
+    {
+        m_ARPlaneManager.enabled = !m_ARPlaneManager.enabled;
+
+        if (m_ARPlaneManager.enabled)
+        {
+            SetAllPlanesActiveOrDeactive(true);
+            GetComponent<ARPlacer>().enabled = true;
+            buttonText.text = "Disable Plane Detection And Hide Existing";
+        }else
+        {
+            SetAllPlanesActiveOrDeactive(false);
+            GetComponent<ARPlacer>().enabled = false;
+            buttonText.text = "Disable Plane Detection And Show Existing";
+        }
+    }
+
+    void SetAllPlanesActiveOrDeactive(bool value)
+    {
+        foreach (var plane in m_ARPlaneManager.trackables)
+        {
+            plane.gameObject.SetActive(value);
+        }
+    }
+}
